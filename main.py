73ea85f67698268b7476e764c216fb9c504e8b48
Dmitriy Dark <d.dark@socialqauntum.com>
2021-07-16 00:40:13 +0300

    Написал базовый подсчёт времени

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import datetime
 
 time = datetime.datetime.now()
-relationships_start = datetime.datetime(year=2021,month=7,day=2)
+relationships_start = datetime.datetime(year=2021,month=7,day=2,second=time.second,microsecond=time.microsecond)
 print(time)
 print(relationships_start)
 diff_time = time - relationships_start
-print(diff_time)
+print("Our relationships timer was start", diff_time, "min")
