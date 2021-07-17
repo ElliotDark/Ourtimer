4efa5c5b3ea1e07f340b5b47287b7323fdd915ab
Dmitriy Dark <d.dark@socialqauntum.com>
2021-07-18 00:51:07 +0300

    Фикшено. Вернул микросекунды, так прикольнее ^^

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import datetime
 
 time = datetime.datetime.now()
-relationships_start = datetime.datetime(year=2021,month=7,day=2,second=time.second,microsecond=time.microsecond)
-print(time)
-print(relationships_start)
+relationships_start = datetime.datetime(year=2021,month=7,day=2)
+
 diff_time = time - relationships_start
-print("Our relationships timer was start", diff_time, "min")
+print("Our relationships timer was start", diff_time)
